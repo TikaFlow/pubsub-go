8d5a56c6c091334ef09c70855f984ae0a5ab9719
TikaFlow <tika_flow@qq.com>
2026-05-30 18:58:16 +0800

    refactor(pubsub): remove unused Bus interface and io import
    删除了未使用的Bus接口定义以及多余的io包导入，清理冗余代码

--- pubsub.go
+++ pubsub.go
@@ -2,7 +2,6 @@ package pubsub
 
 import (
 	"errors"
-	"io"
 	"sync/atomic"
 
 	pool "github.com/TikaFlow/worker-pool"
@@ -25,16 +24,6 @@ type subscription struct {
 	topic   string
 	handler Handler
 	once    bool
-}
-
-// Bus 数据总线接口
-type Bus interface {
-	io.Closer
-	Subscribe(topic string, handler Handler) (SubscriptionID, error)
-	SubscribeOnce(topic string, handler Handler) (SubscriptionID, error)
-	Publish(topic string, message any) error
-	UnSubscribe(id SubscriptionID)
-	UnSubscribeAll()
 }
 
 type subscriptionMap map[SubscriptionID]*subscription
